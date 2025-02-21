ce966260afba49b7b3f235414e3d0f817121b741
Jurgis Megnis <jurgis@Jurgiss-MacBook-Pro.local>
2025-02-21 12:58:42 +0200

    Added Lottie

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { provideCacheableAnimationLoader, provideLottieOptions } from 'ngx-lottie';
+import player from 'lottie-web';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(),
+    
+    provideLottieOptions({ player: () => player}),
+    provideCacheableAnimationLoader()
+  ]
 };
